9dbf444e4b8553bbf903b774f6e32f6fdf3dba6b
signumneo <jacobtp28@gmail.com>
2024-07-26 22:50:57 -0700

    Initial Commit

---
+++ StepCounterWidget/StepCounterWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterWidgetApp.swift
+//  StepCounterWidget
+//
+//  Created by Jacob Thomas on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct StepCounterWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
